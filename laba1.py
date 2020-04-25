7288fc4117a5dc7851d0da5015d878175f318904
meerkatinho <64307693+meerkatinho@users.noreply.github.com>
2020-04-25 20:13:41 +1000

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,32 @@
+wrong_answer_counter=0
+flag= True
+while flag:
+    num_1=int(input("first: "))
+    num_2=int(input("second: "))
+    command=input('input command: ')
+    if command=='+':
+        print(num_1 + num_2)
+    elif command=='-':
+        print(num_1 - num_2)
+    elif command=='/':
+        print(num_1 / num_2)
+    elif command=='*':
+        print(num_1 * num_2)
+    else:
+        print('error')
+    while True:
+        command=input('Continue? yes/no ')
+        if command=="no":
+            flag=False
+            break
+        elif command=="yes":
+            wrong_answer_counter = 0
+            break
+        else:
+            print("error")
+            wrong_answer_counter+=1
+            if wrong_answer_counter==3:
+                flag=False
+                print('too much error!')
+                break
+
